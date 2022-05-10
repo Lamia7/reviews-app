17ad9b00cab625228cd9dc69864df13cffcbbbd5
Ellamia <lamia.elralimi@nova-technology.fr>
2022-05-10 09:05:48 +0200

    conf: installed and setup apollo/client & graphQL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 // pages & layouts
 import HomePage from './pages/HomePage';
@@ -6,24 +7,32 @@ import ReviewDetails from './pages/ReviewDetails';
 import Category from './pages/Category';
 import SiteHeader from './components/SiteHeader'
 
+// apollo client
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache()
+})
+
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <SiteHeader />
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/details/:id">
-            <ReviewDetails />
-          </Route>
-          <Route path="/category/:id">
-            <Category />
-          </Route>
-        </Switch>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <SiteHeader />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/details/:id">
+              <ReviewDetails />
+            </Route>
+            <Route path="/category/:id">
+              <Category />
+            </Route>
+          </Switch>
+        </div>
+      </ApolloProvider>
     </Router>
   );
 }
