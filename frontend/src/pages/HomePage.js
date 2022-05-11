b230481d90f132eb5092fa586a3825448b62133c
Ellamia <lamia.elralimi@nova-technology.fr>
2022-05-11 16:13:51 +0200

    feat: GetReviews request with GraphQL

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+import { useQuery, gql  } from '@apollo/client'
+
+const REVIEWS = gql`
+query GetReviews {
+  reviews{
+    data{
+      id,
+      attributes{
+        title,
+        body,
+        rating,
+      }
+    }
+  }
+}
+`
 
 export default function HomePage() {
 
-  const { loading, error, data } = useFetch('http://localhost:1337/api/reviews')
+  const { loading, error, data } = useQuery(REVIEWS)
+  const [reviewsArray, setReviewArray] = useState([])
+  
+
+  useEffect(() => {
+    if (data !== undefined) {
+      // console.log(reviewsArray)
+      setReviewArray(data.reviews.data)
+    }
+  })
 
   return (
     <>
@@ -12,7 +36,7 @@ export default function HomePage() {
     {loading && <p>Loading...</p>}
     {error && <p>Error...</p>}
     <div>
-      {data && data.map((review) => (
+      {reviewsArray.length > 0 && reviewsArray.map((review) => (
         <div key={review.id} className="review-card">
           <div className="rating">{review.attributes.rating}</div>
           <h2>{review.attributes.title}</h2>
